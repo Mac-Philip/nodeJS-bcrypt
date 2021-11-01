5317044b10c4e86153b82f4ac1683185ad3af140
Philip Mac <40393613+amPhilip@users.noreply.github.com>
2021-11-01 12:37:18 +0300

    Backend

---
+++ backend/data.js
@@ -0,0 +1,10 @@
+const users = [
+    //sample password
+  {
+    _id: 1587912698986,
+    email: 'philipmwangi91@gmail.com',
+    password: '$2b$13$CW3SP.VCKI0NQFgu9plKQeXc3KugDB9hD/EB.nDgS79FQyxzBSDBW',
+  },
+];
+
+module.exports = { users };
